ef72929cc2a24e9b0308ef9cfe101658a1ecf29f
Housten <justin.elsner@outlook.de>
2025-11-19 20:55:00 +0100

    Day 3 Challenge 10: Enemy spawner - programmatic node creation

---
+++ 02-godot-basics/enemy_spawner.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_enemies(11, 100)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func spawn_enemies(count: int, spacing: int) -> void:
+	
+	for i in range(count):
+		var enemy = ColorRect.new()
+		enemy.position = Vector2(0, i * spacing)
+		enemy.size = Vector2(50, 50)
+		enemy.color = Color.RED
+		enemy.name = "Enemy_" + str(i)
+		add_child(enemy)
